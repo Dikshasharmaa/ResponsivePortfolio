95a5ad6effaa21c5fa1392d19584e91df4127923
Diksha Sharma <diksha@dikshas-laptop.mynetworksettings.com>
2024-07-09 13:05:16 -0400

    Added css to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './Navbar.module.css';
 
 export const Navbar = () => {
     return (
-       <nav>
-        <a href="/">Portfolio</a>
-        <div>
-            <ul>
+       <nav className={styles.navbar}>
+        <a className={styles.title} href="/">Portfolio</a>
+        <div className= {styles.menu}>
+            <ul className={styles.menuItem}>
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
                 <li><a href="#projects">Projects</a></li>
