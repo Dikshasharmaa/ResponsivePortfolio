0455d1e14f34b080e107de8163826ca20db7e4a2
Diksha Sharma <diksha66sharma78@gmail.com>
2024-07-18 15:03:47 -0400

    Added content to abut component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
+import styles from "./About.module.css";
 
 export const About =()=>{
     return <section>
         <h2>About</h2>
-        <div><img src={getImageUrl("about/aboutImage.png")} alt=""/></div>
+        <div><img src={getImageUrl("about/aboutImage.png")} alt="Me siting with a laptop"/>
+        <ul>
+        <li><img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon"/></li>
+            <div><h3>Front end developer </h3>
+            <p>I'm a front end developer with experience in building responsive and optimized sites</p></div>
+
+            <li><img src={getImageUrl("about/serverIcon.png")} alt="Server Icon"/></li>
+            <div><h3>Backend developer </h3>
+            <p>I have experience in developing fast and optimized and APIs</p></div>
+
+            <li><img src={getImageUrl("about/serverIcon.png")} alt="Server Icon"/></li>
+            <div><h3>UI designer </h3>
+            <p>I have designed multiple landing pages andhave systems as well</p></div>
+        </ul>
+        </div>
     </section>
 };
