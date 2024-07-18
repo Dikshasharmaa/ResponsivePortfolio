4751bc5c0c9333f1d5bcc35aa1fc4c2ed76b058a
Diksha Sharma <diksha66sharma78@gmail.com>
2024-07-18 15:09:44 -0400

    Added classnames to all the tags

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,20 +3,20 @@ import { getImageUrl } from "../../utils";
 import styles from "./About.module.css";
 
 export const About =()=>{
-    return <section>
-        <h2>About</h2>
-        <div><img src={getImageUrl("about/aboutImage.png")} alt="Me siting with a laptop"/>
-        <ul>
-        <li><img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon"/></li>
-            <div><h3>Front end developer </h3>
+    return <section className={styles.container}>
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}><img src={getImageUrl("about/aboutImage.png")} alt="Me siting with a laptop" className={styles.aboutImage}/>
+        <ul className={styles.aboutItems}>
+        <li className={styles.aboutItem}><img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" /></li>
+            <div className={styles.aboutItemText}><h3>Front end developer </h3>
             <p>I'm a front end developer with experience in building responsive and optimized sites</p></div>
 
-            <li><img src={getImageUrl("about/serverIcon.png")} alt="Server Icon"/></li>
-            <div><h3>Backend developer </h3>
+            <li className={styles.aboutItem}><img src={getImageUrl("about/serverIcon.png")} alt="Server Icon"/></li>
+            <div className={styles.aboutItemText}><h3>Backend developer </h3>
             <p>I have experience in developing fast and optimized and APIs</p></div>
 
-            <li><img src={getImageUrl("about/serverIcon.png")} alt="Server Icon"/></li>
-            <div><h3>UI designer </h3>
+            <li className={styles.aboutItem}><img src={getImageUrl("about/uiIcon.png")} alt="UI Icon"/></li>
+            <div className={styles.aboutItemText}><h3>UI designer </h3>
             <p>I have designed multiple landing pages andhave systems as well</p></div>
         </ul>
         </div>
