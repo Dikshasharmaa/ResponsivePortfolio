dde038c756f2d0312b9736d17f110c64fcbadbca
Diksha Sharma <diksha66sharma78@gmail.com>
2024-07-23 16:46:02 -0400

    Working on experince component

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import skills from "../data/skills.json";
+
+export const Experience = () => {
+    return (
+        <section id ="experience">
+            <h2>Experience</h2>
+            <div><div>
+
+            </div>
+                <ul>
+                    
+                </ul>
+            </div>
+        </section>
+    )}
