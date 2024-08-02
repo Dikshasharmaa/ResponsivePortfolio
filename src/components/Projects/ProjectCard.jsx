133e17d1ee3b21983f9500e6323172ebc792a577
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-02 16:09:42 -0400

    Added classnames

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
+import styles from "./ProjectCard.module.css";
 
 export const ProjectCard=({project :{title, imageSrc, description, skills, demo, source}})=>{   //const Project = props.project
-    return <div >
-    <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`}/>
-    <h3>{title}</h3>
-    <p>{description}</p>
-    <ul>
+    return <div className={styles.conatiner}>
+    <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image}/>
+    <h3 className={styles.title}>{title}</h3>
+    <p className={styles.description}>{description}</p>
+    <ul className={styles.skills}>
         {skills.map((skill, id)=>{
-            <li key={id}>{skill}</li>;
+            <li key={id} className={styles.skill}>{skill} </li>;
         })}
     </ul>
-    <div>
-        <a href={demo}>Demo</a>
+    <div className={styles.links}>
+        <a href={demo} className={styles.link}>Demo</a>
         <a href={source}>Source</a>
     </div>
 </div>;
--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import projects from "../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
+import styles from "./projects.modules.css";
 
 export const Projects=()=>{
     return(
-        <section>
-            <h2>
+        <section className={styles.conatiner}>
+            <h2 className={styles.title}>
                 Project
             </h2>
-            <div>
+            <div className={styles.projects}>
                 {projects.map((project, id)=>{
                     return (
                         <ProjectCard key ={id} project={project}></ProjectCard>
