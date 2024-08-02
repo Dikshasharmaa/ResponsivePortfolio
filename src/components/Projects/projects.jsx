c8015eade7c691dc162bc9f7bfa3b668a7bbc031
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-02 15:55:57 -0400

    Made a new file ProjectCard to make code compact

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import projects from "../data/projects.json";
-import { getImageUrl } from "../../utils";
+import { ProjectCard } from "./ProjectCard";
 
 export const Projects=()=>{
     return(
@@ -10,20 +10,9 @@ export const Projects=()=>{
             </h2>
             <div>
                 {projects.map((project, id)=>{
-                    return <div key ={id}>
-                        <img src={getImageUrl(project.imageSrc)} alt={`Image of ${project.title}`}/>
-                        <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                        <ul>
-                            {project.skills.map((skill, id)=>{
-                                <li key={id}>{skill}</li>;
-                            })}
-                        </ul>
-                        <div>
-                            <a href={project.demo}>Demo</a>
-                            <a href={project.source}>Source</a>
-                        </div>
-                    </div>
+                    return (
+                        <ProjectCard key ={id} project={project}></ProjectCard>
+                    );
                 })}
             </div>
         </section>
