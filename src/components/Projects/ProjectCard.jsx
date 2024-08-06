20cd51df34ec535f6815eaddc0991bd6eb864360
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-06 15:34:58 -0400

    Addec css

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -9,12 +9,12 @@ export const ProjectCard=({project :{title, imageSrc, description, skills, demo,
     <p className={styles.description}>{description}</p>
     <ul className={styles.skills}>
         {skills.map((skill, id)=>{
-            <li key={id} className={styles.skill}>{skill} </li>;
+           return( <li key={id} className={styles.skill}>{skill} </li>);
         })}
     </ul>
     <div className={styles.links}>
         <a href={demo} className={styles.link}>Demo</a>
-        <a href={source}>Source</a>
+        <a href={source} className={styles.link}>Source</a>
     </div>
 </div>;
 }
