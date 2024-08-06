b64aafab65b2beeb8a744beababe11966d2463a3
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-06 15:02:21 -0400

    Added Css to project component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {Navbar} from "./components/Navbar";
 import {Hero} from "./components/Hero/Hero";
 import {About} from "./components/About/About";
 import {Experience} from "./components/Experience/Experience";
-import {Projects} from "./components/Projects/projects";
+import {Projects} from "./components/Projects/Project.jsx";
 
 function App() {
   return (
