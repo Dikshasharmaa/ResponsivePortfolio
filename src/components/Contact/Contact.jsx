828bc117cb5e3ac83fe2cde6599d3f1a5464f26b
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-08 16:21:26 -0400

    Added contact information

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,6 +10,15 @@ export const Contact=()=>{
         <ul>
             <li>
                 <img src={getImageUrl("contact/emailIcon.png")} alt="email"/>
+                <a href="mailto:dikshadevelops@gmail.com">dikshadevelops@gmail.com</a>
+            </li>
+            <li>
+                <img src={getImageUrl("contact/linkdinIcon.png")} alt="Linkdin"/>
+                <a href="https://www.linkedin.com/in/dikshasharma6/">linkdIn.com/Diksha</a>
+            </li>
+            <li>
+                <img src={getImageUrl("contact/githubIcon.png")} alt="Github"/>
+                <a href="https://github.com/Dikshasharmaa">github.com/Diksha</a>
             </li>
         </ul>
     </footer>
