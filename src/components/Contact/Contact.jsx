8e38a1160570a79d2bde64b7368e9bb2f826dd58
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-08 17:09:26 -0400

    Give styling to footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
+import styles from "./Contact.module.css";
 
 export const Contact=()=>{
-    return <footer>
-        <div>
+    return <footer id = "contact" className={styles.Container}>
+        <div className={styles.text}>
             <h2>Contact</h2>
             <p>Feel Free to Contact</p>
         </div>
-        <ul>
-            <li>
+        <ul className={styles.links}> 
+            <li className={styles.link}>
                 <img src={getImageUrl("contact/emailIcon.png")} alt="email"/>
                 <a href="mailto:dikshadevelops@gmail.com">dikshadevelops@gmail.com</a>
             </li>
-            <li>
+            <li className={styles.link}>
                 <img src={getImageUrl("contact/linkdinIcon.png")} alt="Linkdin"/>
                 <a href="https://www.linkedin.com/in/dikshasharma6/">linkdIn.com/Diksha</a>
             </li>
-            <li>
+            <li className={styles.link}>
                 <img src={getImageUrl("contact/githubIcon.png")} alt="Github"/>
                 <a href="https://github.com/Dikshasharmaa">github.com/Diksha</a>
             </li>
