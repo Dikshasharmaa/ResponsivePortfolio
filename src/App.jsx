80006dfe4adb2a639a8b6eaab2dc584e2471b7eb
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-12 14:05:00 -0400

    Added source for projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import styles from "./App.modules.css";
-import {Navbar} from "./components/Navbar";
+import {Navbar} from "./components/Navbar/Navbar.jsx";
 import {Hero} from "./components/Hero/Hero";
 import {About} from "./components/About/About";
 import {Experience} from "./components/Experience/Experience";
