c406890cdd4b8bdd52e95dad3f83999512bec3b7
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-20 15:11:29 -0400

    Fixed hero.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,36 +1,51 @@
 import React from "react";
-import { getImageUrl } from "../../utils";
+
 import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
 
-export const About =()=>{
-    return (
-    <section className={styles.container} id ="about">
-        <h2 className={styles.title}>About</h2>
-        <div className={styles.content}>
-            <img src={getImageUrl("about/aboutImage.png")} 
-            alt="Me siting with a laptop" className={styles.aboutImage}/>
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
         <ul className={styles.aboutItems}>
-        <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-            <h3>Front end developer </h3>
-            <p>I'm a front end developer with experience in building responsive and optimized sites</p>
-            </div></li>
-
-            <li className={styles.aboutItem}>
-                <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon"/>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
             <div className={styles.aboutItemText}>
-                <h3>Backend developer </h3>
-            <p>I have experience in developing fast and optimized and APIs</p></div></li>
-
-            <li className={styles.aboutItem}>
-                <img src={getImageUrl("about/uiIcon.png")} alt="UI Icon"/>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
-                <h3>UI designer </h3>
-            <p>I have designed multiple landing pages andhave systems as well</p></div>
-            </li>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li>
         </ul>
-        </div>
+      </div>
     </section>
-    );
+  );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { getImageUrl } from "../../utils";
+
 import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
-    return (
-        <section className={styles.container}>
-            <div className={styles.content}>
-                <h1 className={styles.title}>Hi, I'm Diksha Sharma</h1>
-                <p className={styles.description}>
-                    I'm an entry-level software developer with experience in Java and Python. Reach out if you want to learn more!
-                </p>
-                <a href="mailto:dikshadevelops@gmail.com" className={styles.contactBtn}>
-                    Contact Me
-                </a>
-            </div>
-            <img
-                src={getImageUrl("hero/heroImage.png")}
-                alt="Hero image of me"
-                className={styles.heroImg}
-            />
-            <div className={styles.topBlur}></div>
-            <div className={styles.bottomBlur}></div>
-        </section>
-    );
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Diksha Sharma</h1>
+        <p className={styles.description}>
+        I'm a front end developer with experience in building responsive and optimized sites
+        </p>
+        <a href="mailto:dikshadevelops@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      {/* <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} /> */}
+    </section>
+  );
 };
